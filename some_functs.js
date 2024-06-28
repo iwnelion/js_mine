c59e1b21d49967cbde50ae35333b1027d2da0c90
joana b <153987262+iwnelion@users.noreply.github.com>
2024-06-28 15:43:26 +0300

    Create some_functs.js

---
+++ some_functs.js
@@ -0,0 +1,19 @@
+function add7(num){
+    return num+7
+}
+console.log(add7(10))
+
+function multiply(n1,n2){
+    return n1*n2
+}
+console.log(multiply(10,20))
+
+function capitalize(txt){
+    return txt.toUpperCase()
+}
+console.log(capitalize("meow"))
+
+function lastletter(otherTxt){
+    return otherTxt.slice(-1)
+}
+console.log(lastletter("cat"))
