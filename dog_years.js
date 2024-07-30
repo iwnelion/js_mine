b3a8fd54ae0b6acfde8539a83345f27d80f3ef02
joana b <153987262+iwnelion@users.noreply.github.com>
2024-07-30 15:34:58 +0300

    Create dog_years.js

---
+++ dog_years.js
@@ -0,0 +1,15 @@
+//my age in a variable
+const myAge=23;
+//a variable that will change
+let earlyYears=2;
+earlyYears*=10.5;
+//a variable that will change
+let laterYears=myAge-2;
+//dog years
+laterYears*=4;
+//ny age in dog years
+myAgeInDogYears=earlyYears+laterYears;
+//my name to lower case
+let myName='JOANA'.toLowerCase();
+//prints the result
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
